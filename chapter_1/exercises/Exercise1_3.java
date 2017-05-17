7ba69582bbb69e51e71c57a494d9d13d86651f35
Brian McEntee <brian.m.mcentee@gmail.com>
2017-05-16 21:21:58 -0400

    Add completed exercise 1_3

---
+++ chapter_1/exercises/Exercise1_3.java
@@ -0,0 +1,13 @@
+public class Exercise1_3 {
+    public static void main(String[] ags) {
+        String l1 = "    J    A    V     V    A";
+        String l2 = "    J   A A    V   V    A A";
+        String l3 = "J   J  AAAAA    V V    AAAAA";
+        String l4 = " J J  A     A    V    A     A";
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+    }
+}
